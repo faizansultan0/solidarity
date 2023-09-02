4289d8e09afe1914b3ba6bed2b631b061de69fd9
Sultan Faizan Bahoo <faizansultan113@gmail.com>
2023-09-02 15:03:53 +0500

    Responsive Terms of Use Page Completed

--- src/Components/Terms.jsx
+++ src/Components/Terms.jsx
@@ -1,8 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import './Terms.css';
+import TermsReceived from '../Data/terms.json';
+
 const Terms = () => {
+    const TermsData = TermsReceived;
     return (
-        <>
-            This is Terms Page
-        </>
+        <section className="terms">
+            <Container>
+                <div className="content">
+                    <h1 className="hero-heading">Terms of Use</h1>
+
+                    {TermsData.map((term, index) => (
+                        <div className='Term' key={index}>
+                            <h2 className="terms-heading"> {index !== 0? index + 1 + '. ' : null} { term.title }</h2>
+                            <p className="terms-para">{ term.description }</p>
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </section>
     )
 }
 
